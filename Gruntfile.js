1982d0d4d890b68e53c60234315f98adb020bbbf
Ryudo Nakagawa <nkgw.ry@gmail.com>
2014-12-02 16:32:57 +0900

    install bootstrap and jquery

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,20 @@
 module.exports = function(grunt) {
-
+    'use strict';
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
-        },
         bower: {
-            dev: {
-                dest: 'public/',
-                js_dest: 'public/js/',
-                css_dest: 'public/css/',
-                fonts_dest: 'public/fonts/',
+            install: {
                 options: {
-                    expand: true
+                    targetDir: './public/bower',
+                    layout: 'byType',
+                    install: true,
+                    verbose: false,
+                    cleanTargetDir: true,
+                    cleanBowerDir: false
                 }
             }
         }
     });
-    grunt.loadNpmTasks('grunt-bower');
-    // Default task(s).
-    grunt.registerTask('default', ['bower']);
-
+    grunt.loadNpmTasks('grunt-bower-task');
 };
